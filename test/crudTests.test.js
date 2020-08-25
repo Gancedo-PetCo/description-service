e1d0c2feb168e5e9f89df054804ca93fb6d60bb0
Eric Zepeda <zepedaeric@gmail.com>
2020-08-25 13:05:49 -0700

    CRUD Tests

--- test/crudTests.test.js
+++ test/crudTests.test.js
@@ -4,3 +4,37 @@ const app = require('../server.js');
 const supertest = require('supertest');
 const request = supertest(app);
 import 'babel-polyfill';
+
+describe('CRUD Endpoint Tests', () => {
+  beforeAll(async () => {
+    await mongoose.connect(
+      'mongodb://localhost/description_directions_attributes',
+      { useNewUrlParser: true, useCreateIndex: true },
+      (err) => {
+        if (err) {
+          console.error(err);
+          process.exit(1);
+        }
+      }
+    );
+  });
+
+  afterAll(async (done) => {
+    await mongoose.connection.close((err) => {
+      if (err) {
+        console.log('error closing mongoose connection: ', err);
+      } else {
+        done();
+      }
+    });
+  });
+
+  it('posts to the /descriptionObject endpoint', async (done) => {
+    const oldCount = await db.Description.count({});
+    const response = await request.post('/descriptionObject/');
+    const newCount = await db.Description.count({});
+    expect(response.status).toBe(200);
+    expect(newCount).toEqual(oldCount + 1);
+    done();
+  });
+});
