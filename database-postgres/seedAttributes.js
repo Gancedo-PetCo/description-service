b7ee30c582ca23a8e61dfaff450195c05f208696
Eric Zepeda <zepedaeric@gmail.com>
2020-08-27 19:12:39 -0700

    Fix paths for grabbing csv files

--- database-postgres/seedAttributes.js
+++ database-postgres/seedAttributes.js
@@ -23,7 +23,7 @@ let csvStream = fastcsv
     });
 
     const query =
-      'INSERT INTO attributes (id, "primaryColor", "material", "length", "width", "data") VALUES ($1, $2, $3, $4, $5, $6)';
+      'INSERT INTO attributes (id, "primaryColor", "material", "length", "width", "descriptionId") VALUES ($1, $2, $3, $4, $5, $6)';
     pool.connect((err, client, done) => {
       if (err) throw err;
       try {
