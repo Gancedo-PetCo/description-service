3b69e2171317bfa0b7d87416e15480bd02ee0809
Eric Zepeda <zepedaeric@gmail.com>
2020-09-01 00:19:00 -0700

    Notes for generation file, change package json script for generating data

--- database-postgres/generateSeeds.js
+++ database-postgres/generateSeeds.js
@@ -9,34 +9,50 @@ const {
   generateDirectionsShape,
 } = require('./generationScript.js');
 
+//Create Descriptions Write Stream
 const writeDescriptions = fs.createWriteStream(
   path.join(__dirname, '..', 'database-postgres-data', 'descriptions.csv')
 );
+// Write Descriptions Headers
 writeDescriptions.write(
   'title, description, sku, primary_brand, days_to_ship\n',
   'utf8'
 );
+// Generate Descriptions Data
 generateCSVdata(writeDescriptions, 'utf8', 100, generateDescriptionsShape, 99);
 
 ///////
+
+//Create Attributes Write Stream
 const writeAttributes = fs.createWriteStream(
   path.join(__dirname, '..', 'database-postgres-data', 'attributes.csv')
 );
+// Write Attributes Headers
 writeAttributes.write(
   'primary_color, material,  length, width, description_id\n',
   'utf-8'
 );
+// Generate Attributes Data
 generateCSVdata(writeAttributes, 'utf8', 100, generateAttributesShape, 99);
+
 ////////
+
+//Create Directions Write Stream
 const writeDirections = fs.createWriteStream(
   path.join(__dirname, '..', 'database-postgres-data', 'directions.csv')
 );
+// Write Directions Headers
 writeDirections.write('directions, description_id\n', 'utf8');
+// Generate Directions Data
 generateCSVdata(writeDirections, 'utf8', 100, generateDirectionsShape, 99);
+
 ////////
+
+//Create Details Write Stream
 const writeDetails = fs.createWriteStream(
   path.join(__dirname, '..', 'database-postgres-data', 'details.csv')
 );
+// Write Details Headers
 writeDetails.write('additional_details, description_id\n', 'utf8');
-
+// Generate Details Data
 generateCSVdata(writeDetails, 'utf8', 100, generateDetailsShape, 99);
